6a027060fc06d3d85dbd1d21f40bc6d63191fb5d
niat786 <niat786@gmail.com>
2023-03-27 17:17:36 +0500

    api version 4

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import boto3
 import os
 import re
+from io import BytesIO
 
 
 
@@ -68,20 +69,64 @@ def download_upload(url,file_name, chunk_size=1024):
     except requests.exceptions.RequestException:
         return {'status':404}
     else:
-        # Set the chunk size based on the file size
-        chunk_size = 0
-        if file_size > 0:
-            chunk_size = max(chunk_size, file_size // (1024 * 10))
 
-        # Download the file in chunks
-        with open(file_name, "wb") as file:
+        s3 = boto3.client('s3', endpoint_url=ENDPOINT)
+         # Set the chunk size (e.g., 5 MB)
+        chunk_size = 5 * 1024 * 1024
+
+        # Define the bucket and key
+        bucket_name = BUCKET_NAME
+        key_name = 'file-streaming'
+
+        # Stream and upload the file in chunks
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+
+            # Create a new multipart upload
+            upload = s3.create_multipart_upload(Bucket=bucket_name, Key=key_name)
+            parts = []
+            part_number = 1
+
+            # Read the content in chunks
             for chunk in response.iter_content(chunk_size=chunk_size):
-                file.write(chunk)
+                # Upload each chunk to S3
+                result = s3.upload_part(
+                    Body=chunk,
+                    Bucket=bucket_name,
+                    Key=key_name,
+                    UploadId=upload['UploadId'],
+                    PartNumber=part_number
+                )
 
-        # Upload the file to Backblaze B2 using the S3-compatible API
-        with open(file_name, "rb") as f:
-            s3.upload_fileobj(f, "apkeve", file_name)
+                # Save the ETag and part number for completing the upload
+                parts.append({'ETag': result['ETag'], 'PartNumber': part_number})
+                part_number += 1
 
-        # Delete the local file
-        os.remove(file_name)
-        return {'status': 200, 'file_size': file_size, 'file_name': file_name}
+            # Complete the multipart upload
+            s3.complete_multipart_upload(
+                Bucket=bucket_name,
+                Key=key_name,
+                UploadId=upload['UploadId'],
+                MultipartUpload={'Parts': parts}
+            )
+
+        return {"message": "File uploaded successfully"}
+
+
+        # # Set the chunk size based on the file size
+        # chunk_size = 0
+        # if file_size > 0:
+        #     chunk_size = max(chunk_size, file_size // (1024 * 10))
+
+        # # Download the file in chunks
+        # with open(file_name, "wb") as file:
+        #     for chunk in response.iter_content(chunk_size=chunk_size):
+        #         file.write(chunk)
+
+        # # Upload the file to Backblaze B2 using the S3-compatible API
+        # with open(file_name, "rb") as f:
+        #     s3.upload_fileobj(f, "apkeve", file_name)
+
+        # # Delete the local file
+        # os.remove(file_name)
+        # return {'status': 200, 'file_size': file_size, 'file_name': file_name}
